e62aa82d64ee1d6abaa88fa024494c7d618cef2d
Taylor Williams <taylor@thewilliams.net>
2018-05-02 19:38:38 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,5 +1,4 @@
 # code your #position_taken? method here!
 def position_taken? (board, index)
-  board_space = board[index]
-  board_space != " "  && board_space != "" && board_space != nil
+  board[index] != " "  && board[index] != "" && board[index] != nil
 end
